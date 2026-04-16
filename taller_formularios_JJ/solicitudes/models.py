8d14759a015942c9afedeb512434c27d0af16eeb
JuanJoseP-B <jjperezbravo123@gmail.com>
2026-04-16 11:18:18 -0500

    Feature: Modelo y formulario de solicitudes con manejo de archivos

--- taller_formularios_JJ/solicitudes/models.py
+++ taller_formularios_JJ/solicitudes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Solicitud(models.Model):
+    TIPO_SOLICITUD = [
+        ('ACAD', 'Académica'),
+        ('ADMIN', 'Administrativa'),
+        ('TECN', 'Técnica'),
+        ('OTRA', 'Otra'),
+    ]
+
+    nombre_solicitante = models.CharField(max_length=150)
+    documento_identidad = models.CharField(max_length=50)
+    correo = models.EmailField()
+    telefono = models.IntegerField()  # Campo numérico
+    tipo_solicitud = models.CharField(
+        max_length=5,
+        choices=TIPO_SOLICITUD,
+        default='ACAD',
+    )
+    asunto = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha_solicitud = models.DateField()
+    archivo_adjunto = models.FileField(upload_to='adjuntos/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nombre_solicitante} - {self.asunto}"
